62626f2b061661ec135fea40feac8fca2187ee71
AssistioTech <mail@assistio.tech>
2025-04-27 18:06:37 +0330

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -104,7 +104,7 @@
             gap: 8px;
             width: 100%;
             padding: 16px 24px;
-            background: linear-gradient(135deg, var(--chat--color-primary) 0%, var(--chat--color-secondary) 100%);
+            background: var(--chat--color-secondary);
             color: white;
             border: none;
             border-radius: 8px;
@@ -203,7 +203,7 @@
         }
 
         .n8n-chat-widget .chat-input button {
-            background: linear-gradient(135deg, var(--chat--color-primary) 0%, var(--chat--color-secondary) 100%);
+            background: var(--chat--color-secondary);
             color: white;
             border: none;
             border-radius: 8px;
